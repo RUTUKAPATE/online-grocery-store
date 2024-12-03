140178a46e688d9e48285b3911e92796ad8f70ad
Rutuja Kapate <rutuja16kapate@gmail.com>
2024-12-03 16:34:08 +0530

    Added Header

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import {Outfit} from "next/font/google";
 import "./globals.css";
+import Header from "./_components/Header";
 
 const outfit = Outfit({ subsets: ["latin"] });
 
@@ -12,7 +13,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={outfit.className}>{children}
+        className={outfit.className}>
+          <Header/>
+          {children}
       </body>
     </html>
   );
