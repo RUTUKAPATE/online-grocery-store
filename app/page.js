ce535bb5fb33ef22a18eb9107f84ad3a401d6ce6
Rutuja Kapate <rutuja16kapate@gmail.com>
2024-12-07 16:13:56 +0530

    added: CategoryList

--- app/page.js
+++ app/page.js
@@ -2,15 +2,20 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import GlobalApi from "./_utils/GlobalApi";
 import Slider from "./_components/Slider";
+import CategoryList from "./_components/CategoryList";
 
 export default async function Home() {
 
   const sliderList= await GlobalApi.getSliders();
 
+  const categoryList = await GlobalApi.getCategoryList();
+
   return (
     <div className="p-5 md:p-10 px-16">
       {/* Sliders */}
       <Slider sliderList={sliderList}/>
+      {/* Category List */}
+      <CategoryList categoryList={categoryList}/>
     </div>
   );
 }
