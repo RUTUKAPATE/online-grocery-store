0cc2f2f9820688b86141fc6fea1d06b8beac420b
Rutuja Kapate <rutuja16kapate@gmail.com>
2024-12-07 14:42:44 +0530

    added: Sliders

--- app/page.js
+++ app/page.js
@@ -1,10 +1,16 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import GlobalApi from "./_utils/GlobalApi";
+import Slider from "./_components/Slider";
 
-export default function Home() {
+export default async function Home() {
+
+  const sliderList= await GlobalApi.getSliders();
+
   return (
-    <div>
-      Home
+    <div className="p-5 md:p-10 px-16">
+      {/* Sliders */}
+      <Slider sliderList={sliderList}/>
     </div>
   );
 }
