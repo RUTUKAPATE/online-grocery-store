c6a3b2f8e275de8cb223a3486c52dd3314063b32
Rutuja Kapate <rutuja16kapate@gmail.com>
2024-12-09 18:56:47 +0530

    added: product List in Home Screen

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 import GlobalApi from "./_utils/GlobalApi";
 import Slider from "./_components/Slider";
 import CategoryList from "./_components/CategoryList";
+import ProductList from "./_components/ProductList";
 
 export default async function Home() {
 
@@ -10,12 +11,16 @@ export default async function Home() {
 
   const categoryList = await GlobalApi.getCategoryList();
 
+  const productList = await GlobalApi.getAllProducts();
+
   return (
     <div className="p-5 md:p-10 px-16">
       {/* Sliders */}
       <Slider sliderList={sliderList}/>
       {/* Category List */}
       <CategoryList categoryList={categoryList}/>
+      {/* Product List */}
+      <ProductList productList={productList}/>
     </div>
   );
 }
