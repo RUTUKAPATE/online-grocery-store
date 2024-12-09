dae30522ae7e5e3804670ff305b1307fa62092d4
Rutuja Kapate <rutuja16kapate@gmail.com>
2024-12-09 21:26:55 +0530

    added: Product Item Details Box

--- app/_components/ProductItem.jsx
+++ app/_components/ProductItem.jsx
@@ -1,11 +1,21 @@
 import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 import React from 'react'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import ProductItemDetail from './ProductItemDetail'
+
 
 function ProductItem({product}) {
   return (
     <div className='p-2 md:p-6 flex flex-col items-center justify-center gap-3 border rounded-lg hover:scale-105 hover:shadow-lg transition-all ease-in-out cursor-pointer'>
-        <Image src={process.env.NEXT_PUBLIC_BACKEND_BASE_URL+product.images[0]?.url} width={500} height={200} alt={product?.name} className='h-[200px] w-[200px] object-contain'/>
+        <Image src={process.env.NEXT_PUBLIC_BACKEND_BASE_URL+product.images[0]?.url} width={500} height={200} alt={product.name} className='h-[200px] w-[200px] object-contain'/>
         <h2 className='font-bold text-lg'>{product.name}</h2>
         <div className='flex gap-3'>
             {product.sellingPrice&&
@@ -13,7 +23,20 @@ function ProductItem({product}) {
             <h2 className={`font-bold text-lg ${product.sellingPrice&&'line-through text-gray-500'}`}>{product.mrp}</h2>
         </div>
 
-        <Button variant="outline" className="text-primary hover:text-white hover:bg-primary">Add to cart</Button>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="outline" className="text-primary hover:text-white hover:bg-primary">Add to cart</Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              
+              <DialogDescription>
+                <ProductItemDetail product={product}/>
+              </DialogDescription>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
+
     </div>
   )
 }
