751fdeef1c3a1448b46fda9b21e0d109d8134f14
Rutuja Kapate <rutuja16kapate@gmail.com>
2024-12-23 12:14:32 +0530

    added: Footer

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import Slider from "./_components/Slider";
 import GlobalApi from "./_utils/GlobalApi";
 import CategoryList from "./_components/CategoryList";
 import ProductList from "./_components/ProductList";
+import Footer from "./_components/Footer";
 
 export default async function Home() {
 
@@ -12,7 +13,7 @@ export default async function Home() {
   const categoryList=await GlobalApi.getCategoryList();
   
   const productList=await GlobalApi.getAllProducts();
-  
+
   return (
     <div className="p-5 md:p-10 px-16">
       {/* Sliders */}
@@ -21,6 +22,10 @@ export default async function Home() {
       <CategoryList categoryList={categoryList}/>
       {/* Product List */}
       <ProductList productList={productList}/>
+      {/* Banner */}
+      <Image src='/banner.png' width={1000} height={300} alt="banner" className="w-full h-[400px] object-contain"/>
+      {/* Footer */}
+      <Footer/>
     </div>
   );
 }
